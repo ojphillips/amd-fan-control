e55cb111e0d53803a1aeab9802fcc169e95f4fc6
Owen Phillips <owski.phillips@gmail.com>
2016-01-20 21:29:25 +0000

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/ojphillips/amd-fan-control/archive/master.zip
-unzip master.zip
-sudo cp amd-fan-control-master/fan-speed.desktop Desktop
-sudo apt-get -y install gksu
-sudo cp amd-fan-control-master/amd-fan /mnt
